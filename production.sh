b6c7a86d564a162c813649c8baab3ff8f4def93f
vyasg84 <111951537+vyasg84@users.noreply.github.com>
2022-09-02 18:20:51 +0530

    Update production.sh

--- production.sh
+++ production.sh
@@ -1,6 +1,11 @@
 #!/bin/bash 
+echo "Removing existing directory"
 rm -rf docker_python_jenkins
+echo "Clone from GitHub"
 git clone https://github.com/vyasg84/docker_python_jenkins.git
+echo "removing image"
 docker image rmi  vyasg84/jenkins-python-demo:1.0
+echo "Pulling new image from DockerHub"
 docker pull vyasg84/jenkins-python-demo:1.0
+echo "Creating container on server"
 docker run -it -d --name docker_python_jenkins vyasg84/jenkins-python-demo:1.0 bash
