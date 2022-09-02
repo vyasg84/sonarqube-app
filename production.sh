a99cf8bd440924b1eb9455efc47679e8434dd9eb
vyasg84 <111951537+vyasg84@users.noreply.github.com>
2022-09-02 15:57:48 +0530

    Create production.sh

---
+++ production.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+'''ssh -o StrictHostKeyChecking=no root@165.22.208.179 "git clone https://github.com/vyasg84/docker_python_jenkins.git" 
+  "docker image rmi  vyasg84/jenkins-python-demo:1.0"
+  "docker pull vyasg84/jenkins-python-demo:1.0"  ''' 
