e2e36ecc8ecb59407f61e3a2ed851bf323e68ff7
vyasg84 <111951537+vyasg84@users.noreply.github.com>
2022-09-02 17:55:05 +0530

    Update production.sh

--- production.sh
+++ production.sh
@@ -1,5 +1,6 @@
-#!/bin/bash
-
-'''ssh -o StrictHostKeyChecking=no root@165.22.208.179 "git clone https://github.com/vyasg84/docker_python_jenkins.git" 
-  "docker image rmi  vyasg84/jenkins-python-demo:1.0"
-  "docker pull vyasg84/jenkins-python-demo:1.0"  ''' 
+#!/bin/bash 
+rm -rf docker_python_jenkins
+git clone https://github.com/vyasg84/docker_python_jenkins.git
+docker image rmi  vyasg84/jenkins-python-demo:1.0
+docker pull vyasg84/jenkins-python-demo:1.0
+docker run -it -d --name docker_python_jenkins vyasg84/jenkins-python-demo:1.0 bash
